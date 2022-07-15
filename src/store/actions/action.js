e6edfd2a7747f061685ca72610294e1e2e282f53
Jose Manuel <castellanogonzalezjosemanuel@gmail.com>
2022-07-15 19:13:58 +0200

    todosReducer

--- src/store/actions/action.js
+++ src/store/actions/action.js
@@ -1,12 +1,20 @@
+let newId = 0;
+
 export const addTodo = (text) => {
   return {
     type: 'ADD_TODO',
-    payload: { text },
+    payload: { text, id: newId++ },
   };
 };
 export const toggleTodo = (id) => {
   return {
     type: 'TOGGLE_TODO',
     payload: { id },
+  };
+};
+export const setVisibilityFilter = (filter) => {
+  return {
+    type: 'SET_VISIBILITY_FILTER',
+    payload: { filter },
   };
 };
---
+++ src/store/reducers/todosReducer.js
@@ -0,0 +1,19 @@
+export const todosReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO': {
+      return [
+        ...state,
+        { text: action.payload.text, id: action.payload.id, completed: false },
+      ];
+    }
+    case 'TOGGLE_TODO': {
+      return state.map((todo) =>
+        state.id === action.payload.id
+          ? { ...todo, completed: !todo.completed }
+          : todo,
+      );
+    }
+    default:
+      return state;
+  }
+};
