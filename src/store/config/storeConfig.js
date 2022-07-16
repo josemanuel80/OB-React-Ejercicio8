f7af21989161fa7e26a6a06efb450a824366b2e9
Jose Manuel <castellanogonzalezjosemanuel@gmail.com>
2022-07-16 12:18:37 +0200

    completado

--- src/store/config/storeConfig.js
+++ src/store/config/storeConfig.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from '../reducers/rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export const createAppStore = () => {
-  let store = configureStore({ reducer: rootReducer });
+  let store = configureStore({ reducer: rootReducer }, composeWithDevTools());
   return store;
 };
